80705fae60034c3a9eb88000e05b65d4e6de42f6
Oju OWO <owooju738@gmail.com>
2023-05-16 14:17:05 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "kdz-40727",
-  url: "https://kdz-40727.botics.co",
-  api: "https://kdz-40727.botics.co/api/v1"
-}
+  "name": "kdz-40727",
+  "url": "https://kdz-40727.botics.co",
+  "api": "https://kdz-40727.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
